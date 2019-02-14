7f9d9aebebc40d1ea9e4b99160b7e927fd51923c
Hiroko Abe <hihoyeahi@gmail.com>
2019-02-14 21:32:29 +0900

    3week_Feb 13/02

---
+++ app.js
@@ -0,0 +1,14 @@
+let mySwiper = new Swiper('.swiper-container',{
+    direction:'vertical',
+    loop:true,
+
+    pagination:{
+    el:'.swiper-pagination',
+ },
+    navigation:{
+        nextEl:'.swiper-button-prev',
+        prevEl:'.swiper-button-next',
+ },
+
+
+});
